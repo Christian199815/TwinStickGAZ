c7d3a1225f0a5e3f333d54c2ff90fb4dc2a4f5d4
Aaron Moulding <529210@student.glu.nl>
2019-05-21 11:05:05 +0200

    Changes

---
+++ TwinStickShooter/Assets/Scripts/KeycardDoor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeycardDoor : MonoBehaviour
+{
+    public static Dictionary<int, Color> keycardColors = new Dictionary<int, Color>();
+    private Rigidbody rb;
+
+    public int neededKeycard;
+
+    private bool isOpened = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        GetComponent<MeshRenderer>().material.color = keycardColors[neededKeycard];
+        rb.isKinematic = true;
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+        print(player);
+
+        if (player != null)
+        {
+            if (player.keycards.Contains(neededKeycard))
+            {
+                isOpened = true;
+                rb.isKinematic = false;
+            }
+        }
+    }
+}
