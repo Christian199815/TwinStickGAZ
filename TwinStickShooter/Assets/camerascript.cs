7754cf67589f3d150165d2001677ce7500e2a44e
Christian Donker <530300@student.glu.nl>
2019-06-23 22:38:20 +0200

    shit

---
+++ TwinStickShooter/Assets/camerascript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerascript : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraMovement();
+    }
+
+    void CameraMovement()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float zAxisValue = Input.GetAxis("Vertical");
+        if(Camera.current != null)
+        {
+            Camera.current.transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+        }
+    }
+}
